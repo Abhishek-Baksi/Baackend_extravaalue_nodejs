7c14418a3973230e07fac6d5deb0d9201c45d9f4
Abhishek-Baksi <abhisek.baksi@icloud.com>
2023-06-08 03:22:24 +0530

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.extraavalue.com',
+    'https://extraavalue.com'
+]
+
+module.exports = allowedOrigins
---
+++ models/User.js
@@ -0,0 +1,1 @@
+//create userscema here
